093d175c73d2e40773c061b71cd92b5efc4e5f50
Venky8790 <93594741+Venky8790@users.noreply.github.com>
2021-11-08 16:18:39 +0530

    b,kFN bi,f
    ds mhj,avn ,YKUVwukaefnk,hilvmnmabv,uvgliy7y

---
+++ autologin.ps1
@@ -0,0 +1,17 @@
+﻿$ie=New-Object -ComObject "internetexplorer.Application"
+$ie.Visible = $true
+
+Start-Sleep -Seconds 10
+$ie.Navigate("https://www.facebook.com/")
+
+while($ie.busy -eq $true)
+{
+Start-Sleep -Seconds 1
+Write-Host "loading the console...... please wait..."
+}
+Start-Sleep -Seconds 10
+$username = $ie.Document.IHTMLDocument3_getElementById('email')
+$username.value="pavankumar.nagamalla@gmail.com"
+$password = $ie.Document.IHTMLDocument3_getElementById('pass')
+$password.value="pavan@143"
+$ie.Document.IHTMLDocument3_getElementById('u_0_b').click()
